1d5c4352237cef6fa8efcdad6dee7f9c39e2c1f0
deivithsan <deivith.1@hotmail.com>
2016-10-03 23:28:50 -0500

    Update form.php

--- production/form.php
+++ production/form.php
@@ -300,12 +300,15 @@
                     $privileg = $_POST["privilegio"];
                     $pass = (int) $contrase;
                     $priv = (int) $privileg;
+                    $val = 0;
                     $validar2 = "SELECT nombreuser from public.usuarios";
                     $busqueda2 =pg_query($validar2);
                     while ($comparar2 = pg_fetch_array($busqueda2)){
                      if ($comparar2 ["nombreuser"] == $nomus2){
                     echo "<script>alert('El nombre de usuario ya existe')</script>";
-                        }else {
+                       $val=1
+                        }
+                      if($val = 0){
                     $result2 =pg_query($cnx, "INSERT INTO public.usuarios (nombreuser, contraseña, privilegio) VALUES('$nomus2', '$pass', '$priv');");
                     echo"<script>alert('Registrio Agregado Correctamente')</script>";
                   }
@@ -313,7 +316,7 @@
                   }         
           
                  }
-                   }
+                   
                      ?>
 
                       <div class="col-md-12 col-sm-9 col-xs-12 form-group has-feedback">
