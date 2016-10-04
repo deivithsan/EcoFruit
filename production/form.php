b10dc264bb124155e804004a980917db3c56cc8f
deivithsan <deivith.1@hotmail.com>
2016-10-03 23:40:26 -0500

    Update form.php

--- production/form.php
+++ production/form.php
@@ -304,10 +304,13 @@
                     $validar2 = "SELECT nombreuser from public.usuarios";
                     $busqueda2 =pg_query($validar2);
                     while ($comparar2 = pg_fetch_array($busqueda2)){
-                      if ($comparar2 ["nombreuser"] == $nomus2)
+                      if ($comparar2 ["nombreuser"] == $nomus2){
                         echo "<script>alert('El nombre de usuario ya existe')</script>";
                         $val = 1;
+                      }else{
+                        $val = 0;
                       }
+                    }
                       if ($val = 0) {
                         $result2 =pg_query($cnx, "INSERT INTO public.usuarios (nombreuser, contraseña, privilegio) VALUES('$nomus2', '$pass', '$priv');");
                         echo"<script>alert('Registrio Agregado Correctamente')</script>";
