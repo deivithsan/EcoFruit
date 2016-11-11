fd838770487827ecfac10f1a73d22b3edc7656c0
Deivith Becerra <deivith.1@hotmail.com>
2016-11-10 21:55:03 -0500

    Update modInfo.php

--- production/modInfo.php
+++ production/modInfo.php
@@ -383,7 +383,7 @@
                       <div class="ln_solid"></div>
                       <div class="form-group">
                         <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                        <input type="submit" class="btn btn-success" name="Editar" id="Editar">
+                        <input type="submit" class="btn btn-success" name="Enviar" id="Enviar" value="Editar">
                         <button onclick='limpiar()' class="btn btn-success">Limpiar</button>
                         </div>
                       </div>
