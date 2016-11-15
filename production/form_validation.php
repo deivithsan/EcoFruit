fb0e74c5fed0ef3249fb54d631a6d94478f9d7cd
Deivith Becerra <deivith.1@hotmail.com>
2016-11-14 22:16:56 -0500

    Update form_validation.php

--- production/form_validation.php
+++ production/form_validation.php
@@ -221,13 +221,13 @@
                         </div>
                       </div>
                       <div class="item form-group">
-                      <label class="control-label col-md-4 col-sm-3 col-xs-12" for="last-name">Costo <span class="required"></span>
+                      <label class="control-label col-md-4 col-sm-3 col-xs-12" for="last-name">Costo ($) <span class="required"></span>
                       </label>
                       <div class="col-md-6 col-sm-6 col-xs-12">
                         <input type="number" id="costo" name="costo" required="required" class="form-control col-md-7 col-xs-12">
                       </div>
                       </div>
-                      <label class="control-label col-md-4 col-sm-3 col-xs-12" for="last-name">Venta <span class="required"></span>
+                      <label class="control-label col-md-4 col-sm-3 col-xs-12" for="last-name">Venta ($) <span class="required"></span>
                       </label>
                       <div class="col-md-6 col-sm-6 col-xs-12">
                         <input type="number" id="venta" name="venta" required="required" class="form-control col-md-7 col-xs-12">
