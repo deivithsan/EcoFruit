4d86c546657bea49afb810a355a3b2ed5afc11af
Deivith Becerra <deivith.1@hotmail.com>
2017-05-28 16:03:45 -0500

    Add files via upload

--- conexlog.php
+++ conexlog.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$host = 'ec2-54-243-200-159.compute-1.amazonaws.com';
-$user = 'ithxzpubsdyssh';
-$passwd = 'yzRs8R1aJkymNawqYGJkS_4ySJ';
-$db = 'd3q71k9f5t7k2c';
+$host = 'localhost';
+$user = 'postgres';
+$passwd = 'liz6625382';
+$db = 'postgres';
 $port = '5432';
 $strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd sslmode='allow'" ;
 $cnx = pg_connect($strCnx) or die (print "Error de conexion. ");
@@ -19,11 +19,13 @@ echo "Campos vacios";
 	$busqueda=pg_query($sql);
 	$compara = pg_fetch_array($busqueda);
 		if ($compara["nombreuser"] ==$nom && $compara["contraseña"]==$pasencrip){
+			$_SESSION["user"] = $compara['nombreuser'];
 		$sqlpriv = "SELECT privilegio.privil FROM public.usuarios, public.privilegio WHERE usuarios.nombreuser = '$nom' and usuarios.privilegio=privilegio.privil";
 				$busprov = pg_query($sqlpriv);
 				$igual = pg_fetch_array($busprov);
+				$_SESSION["privil"] = $igual['privil'];
 					if ($igual["privil"]== 1){
-					header('Location: production/index.html');
+					header('Location: production/index.php');
 					}elseif ($igual["privil"]== 2){
 						header('Location: comprador.php');
 					}elseif ($igual["privil"]== 3){
