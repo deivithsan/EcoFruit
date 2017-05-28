0e8201c6f2a7869fe7735b9a0227e7322860acf4
Deivith Becerra <deivith.1@hotmail.com>
2017-05-28 16:56:08 -0500

    Update conexlog.php

--- conexlog.php
+++ conexlog.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$host = 'localhost';
-$user = 'postgres';
-$passwd = 'liz6625382';
-$db = 'postgres';
+$host = 'ec2-54-243-200-159.compute-1.amazonaws.com';
+$user = 'ithxzpubsdyssh';
+$passwd = 'yzRs8R1aJkymNawqYGJkS_4ySJ';
+$db = 'd3q71k9f5t7k2c';
 $port = '5432';
 $strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd sslmode='allow'" ;
 $cnx = pg_connect($strCnx) or die (print "Error de conexion. ");
