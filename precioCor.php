478311bbc6d5d8a669f269373fce808e9db5ed4f
Deivith Becerra <deivith.1@hotmail.com>
2017-08-05 21:30:39 -0500

    Update precioCor.php

--- precioCor.php
+++ precioCor.php
@@ -136,7 +136,7 @@
                                     $trs = $html->getElementsByTagName("tr");
                                     ?>
                                     <tr>
-					    <h3>Informacion: los productos con precio 0$ es porque no se encuentran en cosecha</h3>    
+					    <h1>Informacion: los productos con precio 0$ es porque no se encuentran en cosecha</h1>    
                                         <th>Nombre</th>
                                         <th>Presentación</th>
                                         <th>Cantidad</th>
