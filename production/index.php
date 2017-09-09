56302cd76b8284720ec39046d7d172e553158493
Deivith Becerra <deivith.1@hotmail.com>
2017-09-09 09:02:33 -0500

    Update index.php

--- production/index.php
+++ production/index.php
@@ -78,22 +78,22 @@ $apellido = $row["apellido"];
                 <ul class="nav side-menu">
                   <li><a href="index.php"><i class="fa fa-home"></i> Inicio </a>
                   </li>
-                  <li><a><i class="fa fa-edit"></i> Agregar <span class="fa fa-chevron-down"></span></a>
+                  <li><a><i class="fa fa-edit"></i> Formularios <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="form.php">Información Usuario</a></li>
-                      <li><a href="form_validation.php">Productos</a></li>
-                      <li><a href="formPriv.php">Privilegios</a></li>
+                      <li><a href="form.php">Ingresar Información Usuario</a></li>
+                      <li><a href="form_validation.php">Ingresar Productos</a></li>
+                      <li><a href="formPriv.php">Ingresar Privilegios</a></li>
                     </ul>
                   </li>
-                  <li><a><i class="fa fa-table"></i> Visualizar <span class="fa fa-chevron-down"></span></a>
+                  <li><a><i class="fa fa-table"></i> Visualizar Tablas <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="tableUsers.php"> Usuarios </a></li>
+                      <li><a href="tableBuy.php"> Compras </a></li>                      
                       <li><a href="tableInfoUsr.php"> Información de Usuarios </a></li>
                       <li><a href="tableProDisp.php"> Productos </a></li>
                       <li><a href="tableEstateProd.php"> Estado de los Productos </a></li>
-                      <li><a href="tableInfoPriv.php"> Privilegios </a></li>
-                      <li><a href="tableBuy.php"> Compras </a></li>
-                      <li><a href="tableMen.php"> Mensajes </a></li>
+                      <li><a href="tableMen.php"> Mensajes </a></li>  
+                      <li><a href="tableInfoPriv.php"> Privilegios </a></li>                                          
+                      <li><a href="tableUsers.php"> Usuarios </a></li>  
                     </ul>
                   </li>
                   <li><a><i class="fa fa-edit"></i> Modificar Datos <span class="fa fa-chevron-down"></span></a>
