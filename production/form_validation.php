bc2b9bdddfb52dbb2226063c86e61593605d81fb
Deivith Becerra <deivith.1@hotmail.com>
2017-09-21 15:56:56 -0500

    Update form_validation.php

--- production/form_validation.php
+++ production/form_validation.php
@@ -253,6 +253,7 @@ $apellido = $row["apellido"];
                         <input type="number" id="costo" name="costo" required="required" class="form-control col-md-7 col-xs-12">
                       </div>
                       </div>
+                      <div class="item form-group">
                       <label class="control-label col-md-4 col-sm-3 col-xs-12" for="last-name">Venta <span class="required"></span>
                       </label>
                       <div class="col-md-6 col-sm-6 col-xs-12">
