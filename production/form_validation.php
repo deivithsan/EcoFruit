d3ad84bb29b14ed1df6cac3935a9dceb6c1594b6
Deivith Becerra <deivith.1@hotmail.com>
2017-09-21 16:49:18 -0500

    Update form_validation.php

--- production/form_validation.php
+++ production/form_validation.php
@@ -271,7 +271,7 @@ $apellido = $row["apellido"];
                       <div class="form-group">
                         <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
                           <input type="submit" class="btn btn-success">
-                          <button onclick='limpiar()' class="btn btn-success">Limpiar</button>
+                          <button onclick='limpiar3()' class="btn btn-success">Limpiar</button>
                           </div>
                         </div>
                         <?php
@@ -279,8 +279,12 @@ $apellido = $row["apellido"];
                         ?>
                         <script language=javascript>
                         function limpiar3(){
-                          document.getElementById('nombrepriv').value = "";
-                          document.getElementById('numpriv').value = "";
+                          document.getElementById('nomprod').value = "";
+                          document.getElementById('tip').value = "";
+                          document.getElementById('cant').value = "";
+                          document.getElementById('costo').value = "";
+                          document.getElementById('venta').value = "";
+                          document.getElementById('ubicacion').value = "";
                           }
                       </script>
                     </form>
