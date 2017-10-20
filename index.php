1fcba866926aa0bd7b655fdb501327ebb6eee28a
Deivith Becerra <deivith.1@hotmail.com>
2017-10-20 10:47:28 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -262,7 +262,6 @@
 			</div>
 		</section>
 
-
 		<!-- Main jQuery -->
         <script src="js/jquery-1.11.1.min.js"></script>
 		<!-- Single Page Nav -->
@@ -288,18 +287,16 @@
         <script src="js/wow.min.js"></script>
 		<script>
 			var wow = new WOW ({
-				boxClass:     'wow',      // animated element css class (default is wow)
-				animateClass: 'animated', // animation css class (default is animated)
-				offset:       120,          // distance to the element when triggering the animation (default is 0)
-				mobile:       false,       // trigger animations on mobile devices (default is true)
-				live:         true        // act on asynchronously loaded content (default is true)
+				boxClass:     'wow',
+				animateClass: 'animated',
+				offset:       120,
+				mobile:       false,
+				live:         true
 			  }
 			);
 			wow.init();
 		</script>
-		<!-- Custom Functions -->
         <script src="js/custom.js"></script>
-
 		<script type="text/javascript">
 			$(function(){
 				$('#contact-form').validate({
