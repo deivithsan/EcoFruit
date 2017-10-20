54915d2e5011028126ae1183d110b306e90dd0d8
Deivith Becerra <deivith.1@hotmail.com>
2017-10-20 15:55:07 -0500

    Add files via upload

--- precioCor.php
+++ precioCor.php
@@ -16,24 +16,12 @@ if (isset($_SESSION['user'])){
 }
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-    <!-- meta charec set -->
     <meta charset="utf-8">
-    <!-- Always force latest IE rendering engine or request Chrome Frame -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <!-- Page Title -->
-    <title>EcoFruit</title>
-    <!-- Meta Description -->
-
+    <title>Precios Corabastos</title>
     <!-- Mobile Specific Meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Google Font -->
-
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- CSS
@@ -53,30 +41,19 @@ if (isset($_SESSION['user'])){
 
     <!-- Modernizer Script for old Browsers -->
     <script src="js/modernizr-2.6.2.min.js"></script>
-
 </head>
 
 <body id="body">
-
-<!-- preloader -->
-<div id="preloader">
-    <img src="img/Fruta.gif" alt="Preloader">
-</div>
-<!-- end preloader -->
-
-<!--
-Fixed Navigation
-==================================== -->
-<header id="navigation" class="navbar-fixed-top navbar">
-    <div class="container">
-        <div class="navbar-header">
-            <!-- responsive nav button -->
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <i class="fa fa-bars fa-2x"></i>
-            </button>
-
-            <!-- main nav -->
+    <div id="preloader">
+        <img src="img/Fruta.gif" alt="Preloader">
+    </div>
+    <header id="navigation" class="navbar-fixed-top navbar">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <i class="fa fa-bars fa-2x"></i>
+                </button>
             <nav class="collapse navbar-collapse navbar-right" role="navigation">
                 <ul id="nav" class="nav navbar-nav">
                     <li class="current"><a href="#body">Inicio</a></li>
@@ -109,106 +86,78 @@ Fixed Navigation
                     <li><a></a></li>
                     <li><a></a></li>
                 </ul>
-
                 <a href="logout.php">Cerrar Sesión</a>
-
-                <?php
-                }else
-                    echo "</ul>"
-                ?>
+                    <?php }else echo "</ul>" ?>
             </nav>
-            <!-- /main nav -->
-
         </div>
-</header>
-<!--
-End Fixed Navigation
-==================================== --><!--
-        Home Slider
-        ==================================== -->
-
-<section id="slider">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-
-        <!-- Indicators bullet -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        </ol>
-        <!-- End Indicators bullet -->
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-
-            <!-- single slide -->
-            <div class="item active" style="background-image: url(img/log3.png);">
-                <div class="carousel-caption">
-                    <h2 data-wow-duration="700ms" data-wow-delay="500ms" class="wow bounceInDown animated"><a href="index.php">EcoFruit</span>!</a></h2>
-                    <h3 data-wow-duration="1000ms" class="wow slideInLeft animated"><span class="color">Venta eficaz, rapida y total de la fruta en su cosecha</span> </h3>
-                    <p data-wow-duration="1000ms" class="wow slideInRight animated">No se debe perder ni una fruta!</p>
-
-                    <ul class="social-links text-center">
-                        <li><a href=""><i class="fa fa-twitter fa-lg"></i></a></li>
-                        <li><a href=""><i class="fa fa-facebook fa-lg"></i></a></li>
-                    </ul>
+    </header>
+    <section id="slider">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active" style="background-image: url(img/log3.png);">
+                    <div class="carousel-caption">
+                        <h2 data-wow-duration="700ms" data-wow-delay="500ms" class="wow bounceInDown animated"><a href="index.php">EcoFruit</span>!</a></h2>
+                        <h3 data-wow-duration="1000ms" class="wow slideInLeft animated"><span class="color">Venta eficaz, rapida y total de la fruta en su cosecha</span> </h3>
+                        <p data-wow-duration="1000ms" class="wow slideInRight animated">No se debe perder ni una fruta!</p>
+                        <ul class="social-links text-center">
+                            <li><a href=""><i class="fa fa-twitter fa-lg"></i></a></li>
+                            <li><a href=""><i class="fa fa-facebook fa-lg"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <!-- end single slide --></div>
-        <!-- End Wrapper for slides --></div>
-</section><!--
-        Features
-        ==================================== -->
-<section id="features" class="features">
-    <div class="container">
-        <div class="row">
-
-            <div class="sec-title text-center mb50 wow bounceInDown animated" data-wow-duration="500ms">
-                <h2>Precios Actuales en Corabastos</h2>
-
-                <div class="devider"></div>
-            </div>
-
-
-            <!-- service item -->
-            <div class="clearfix"></div>
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                    <div class="x_content">
-                        <table id="datatable-buttons" class="table table-striped table-bordered">
-                            <thead>
-                            <?php
-                            $source = file_get_contents('http://www.corabastos.com.co/sitio/historicoApp2/reportes/prueba.php');
-                            libxml_use_internal_errors(true);
-                            libxml_clear_errors();
-                            $html = new DOMDocument();
-                            $html ->loadHTML($source);
-                            $xpath = new DOMXPath($html);
-                            $result = array();
-                            $trs = $html->getElementsByTagName("tr");
-                            ?>
-                            <tr>
-                                <h4>Información: los productos con precio 0 es porque no se encuentran en cosecha</h4>
-                                <th>Nombre</th>
-                                <th>Presentación</th>
-                                <th>Cantidad</th>
-                                <th>Unidad</th>
-                                <th>$Cal. Extra</th>
-                                <th>$Cal. Primera</th>
-                                <th>Valor Por Unidad</th>
-                                <th>Gran. Superficies</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($trs as $tr) {
-                                $title = @trim($tr->getElementsByTagName("td")->item(0)->nodeValue);
-                                $value = @trim($tr->getElementsByTagName("td")->item(1)->nodeValue);
-                                $value2 = @trim($tr->getElementsByTagName("td")->item(2)->nodeValue);
-                                $value3 = @trim($tr->getElementsByTagName("td")->item(3)->nodeValue);
-                                $value4 = @trim($tr->getElementsByTagName("td")->item(4)->nodeValue);
-                                $value5 = @trim($tr->getElementsByTagName("td")->item(5)->nodeValue);
-                                $value6 = @trim($tr->getElementsByTagName("td")->item(6)->nodeValue);
-                                $value7 = @trim($tr->getElementsByTagName("td")->item(7)->nodeValue);
-                                ?>
+        </div>
+    </section>
+    <section id="features" class="features">
+        <div class="container">
+            <div class="row">
+                <div class="sec-title text-center mb50 wow bounceInDown animated" data-wow-duration="500ms">
+                    <h2>Precios Actuales en Corabastos</h2>
+                        <div class="devider"></div>
+                </div>
+                    <div class="clearfix"></div>
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_content">
+                            <table id="datatable-buttons" class="table table-striped table-bordered">
+                                <thead>
+                                    <?php
+                                    $source = file_get_contents('http://www.corabastos.com.co/sitio/historicoApp2/reportes/prueba.php');
+                                    libxml_use_internal_errors(true);
+                                    libxml_clear_errors();
+                                    $html = new DOMDocument();
+                                    $html ->loadHTML($source);
+                                    $xpath = new DOMXPath($html);
+                                    $result = array();
+                                    $trs = $html->getElementsByTagName("tr");
+                                    ?>
+                                    <tr>
+                                        <h4>Información: los productos con precio 0 es porque no se encuentran en cosecha</h4>
+                                        <th>Nombre</th>
+                                        <th>Presentación</th>
+                                        <th>Cantidad</th>
+                                        <th>Unidad</th>
+                                        <th>$Cal. Extra</th>
+                                        <th>$Cal. Primera</th>
+                                        <th>Valor Por Unidad</th>
+                                        <th>Gran. Superficies</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach ($trs as $tr) {
+                                    $title = @trim($tr->getElementsByTagName("td")->item(0)->nodeValue);
+                                    $value = @trim($tr->getElementsByTagName("td")->item(1)->nodeValue);
+                                    $value2 = @trim($tr->getElementsByTagName("td")->item(2)->nodeValue);
+                                    $value3 = @trim($tr->getElementsByTagName("td")->item(3)->nodeValue);
+                                    $value4 = @trim($tr->getElementsByTagName("td")->item(4)->nodeValue);
+                                    $value5 = @trim($tr->getElementsByTagName("td")->item(5)->nodeValue);
+                                    $value6 = @trim($tr->getElementsByTagName("td")->item(6)->nodeValue);
+                                    $value7 = @trim($tr->getElementsByTagName("td")->item(7)->nodeValue);
+                                    ?>
                                 <tr>
                                     <td><?php echo $title ?></td>
                                     <td><?php echo $value ?></td>
@@ -219,109 +168,15 @@ End Fixed Navigation
                                     <td><?php echo $value6 ?></td>
                                     <td><?php echo $value7 ?></td>
                                 </tr>
-                                <?php
-                            }
-                            ?>
+                               <?php } ?>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-</section>
+    </section>
 
-
-
-<!--
-End Features
-==================================== -->	<!-- Essential jQuery Plugins
-		================================================== -->
-<!-- Datatables -->
-<script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-<script src="../vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-<script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="../vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="../vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-<script src="../vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-<script src="../vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-<script src="../vendors/datatables.net-scroller/js/datatables.scroller.min.js"></script>
-<script src="../vendors/jszip/dist/jszip.min.js"></script>
-<script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
-<!-- Datatables -->
-<script>
-    $(document).ready(function() {
-        var handleDataTableButtons = function() {
-            if ($("#datatable-buttons").length) {
-                $("#datatable-buttons").DataTable({
-                    dom: "Bfrtip",
-                    buttons: [
-                        {
-                            extend: "copy",
-                            className: "btn-sm"
-                        },
-                        {
-                            extend: "csv",
-                            className: "btn-sm"
-                        },
-                        {
-                            extend: "excel",
-                            className: "btn-sm"
-                        },
-                        {
-                            extend: "pdfHtml5",
-                            className: "btn-sm"
-                        },
-                        {
-                            extend: "print",
-                            className: "btn-sm"
-                        },
-                    ],
-                    responsive: true
-                });
-            }
-        };
-        TableManageButtons = function() {
-            "use strict";
-            return {
-                init: function() {
-                    handleDataTableButtons();
-                }
-            };
-        }();
-        $('#datatable').dataTable();
-        $('#datatable-keytable').DataTable({
-            keys: true
-        });
-        $('#datatable-responsive').DataTable();
-        $('#datatable-scroller').DataTable({
-            ajax: "js/datatables/json/scroller-demo.json",
-            deferRender: true,
-            scrollY: 380,
-            scrollCollapse: true,
-            scroller: true
-        });
-        $('#datatable-fixed-header').DataTable({
-            fixedHeader: true
-        });
-        var $datatable = $('#datatable-checkbox');
-        $datatable.dataTable({
-            'order': [[ 1, 'asc' ]],
-            'columnDefs': [
-                { orderable: false, targets: [0] }
-            ]
-        });
-        $datatable.on('draw.dt', function() {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_flat-green'
-            });
-        });
-        TableManageButtons.init();
-    });
-</script>
 <!-- Main jQuery -->
 <script src="js/jquery-1.11.1.min.js"></script>
 <!-- Single Page Nav -->
@@ -347,18 +202,16 @@ End Features
 <script src="js/wow.min.js"></script>
 <script>
     var wow = new WOW ({
-            boxClass:     'wow',      // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset:       120,          // distance to the element when triggering the animation (default is 0)
-            mobile:       false,       // trigger animations on mobile devices (default is true)
-            live:         true        // act on asynchronously loaded content (default is true)
+            boxClass:     'wow',
+            animateClass: 'animated',
+            offset:       120,
+            mobile:       false,
+            live:         true
         }
     );
     wow.init();
 </script>
-<!-- Custom Functions -->
 <script src="js/custom.js"></script>
-
 <script type="text/javascript">
 </script>
 </body>
