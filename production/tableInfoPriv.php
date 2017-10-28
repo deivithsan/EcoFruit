3b32fd17a84d87ea8b9a4300a04726d7b798e5a7
Deivith Becerra <deivith.1@hotmail.com>
2017-10-28 15:38:35 -0500

    Add files via upload

--- production/tableInfoPriv.php
+++ production/tableInfoPriv.php
@@ -57,7 +57,14 @@ $apellido = $row["apellido"];
 
                 <div class="profile">
                     <div class="profile_pic">
-                        <img src="images/<?php echo "$nom" ?>.jpg" alt="..." class="img-circle profile_img">
+                        <?php
+                        $dir = "images/$nom.jpg";
+                        $existe = file_exists($dir);
+                        if ($existe == true){  ?>
+                            <img src="images/<?php echo "$nom" ?>.jpg" alt="..." class="img-circle profile_img">
+                        <?php  } else {  ?>
+                            <img src="images/user.jpg" alt="..." class="img-circle profile_img">
+                        <?php  }  ?>
                     </div>
                     <div class="profile_info">
                         <span>Bienvenido,</span>
@@ -124,7 +131,11 @@ $apellido = $row["apellido"];
                     <ul class="nav navbar-nav navbar-right">
                         <li class="">
                             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                <img src="images/<?php echo "$nom" ?>.jpg" alt=""><?php echo "$nombre $apellido" ?>
+                                <?php  if ($existe == true) { ?>
+                                    <img src="images/<?php echo "$nom" ?>.jpg"  alt=""><?php echo "$nombre $apellido" ?>
+                                <?php  } else { ?>
+                                    <img src="images/user.jpg"  alt=""><?php echo "$nombre $apellido" ?>
+                                <?php  } ?>
                                 <span class=" fa fa-angle-down"></span>
                             </a>
                             <ul class="dropdown-menu dropdown-usermenu pull-right">
