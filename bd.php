e29093596c33c489da7e5bf5914b11b4d843233a
Deivith Becerra <deivith.1@hotmail.com>
2017-11-16 21:19:32 -0500

    Add files via upload

--- bd.php
+++ bd.php
@@ -222,8 +222,7 @@ if (isset($_SESSION['user'])){
                             $numced = (int) $numerocedula;
                             $telef = (int) $numerocelular;
                             $agregar =pg_query($cnx, "INSERT INTO public.compra (idprod,nombreprod, estado, cantdisp, costuni, cantbuy, numced, numcel, vendedorprod, comprador) VALUES ($idpr,'$nomprod','$est',$cantdis,$costun,$cantbuy,$numced,$telef,'$vendedor','$nom');");
-                            echo"<script>alert(Compra Realizada Correctamente )</script>";
-                            //echo "$idpr $nomprod $est $cantdis $costun $cantbuy $numced $tel $vendedor $nom" ;
+                            echo"<script>alert('Compra Realizada Correctamente')</script>";
                         }
                     }
                     ?>
