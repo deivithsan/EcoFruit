015a2a16fd203356cc1b72bfc5202181f3dca10b
Deivith Becerra <deivith.1@hotmail.com>
2017-11-16 22:06:25 -0500

    Update adduser.php

--- production/adduser.php
+++ production/adduser.php
@@ -78,6 +78,7 @@ $apellido = $row["apellido"];
                                         </li>
                                         <li><a><i class="fa fa-table"></i> Visualizar Tablas <span class="fa fa-chevron-down"></span></a>
                                             <ul class="nav child_menu">
+                                                <li><a href="tableBuy.php"> Compras </a></li>
                                                 <li><a href="tableInfoUsr.php"> Información de Usuarios </a></li>
                                                 <li><a href="tableProDisp.php"> Productos </a></li>
                                                 <li><a href="tableEstateProd.php"> Estado de los Productos </a></li>
@@ -95,9 +96,8 @@ $apellido = $row["apellido"];
                                         </li>
                                         <li><a><i class="fa fa-money"></i> Ventas <span class="fa fa-chevron-down"></span></a>
                                             <ul class="nav child_menu">
-                                                <li><a href="tableMen.php"> Mensajes </a></li>
-                                                <li><a href="tableBuy.php"> Compras </a></li>
-                                                <li><a href="modBuy.php">Modificar Compras</a></li>
+                                                <li><a href="tableMen.php"> Mensajes </a></li>                                           
+                                                <li><a href="modBuy.php">Compras</a></li>
                                             </ul>
                                         </li>
                                     </ul>
