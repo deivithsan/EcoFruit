66da6f4cd21b2e67bc3e03ce701e5134752125cf
Deivith Becerra <deivith.1@hotmail.com>
2018-01-23 21:27:57 -0500

    Delete conex.php

--- conex.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'ec2-54-243-200-159.compute-1.amazonaws.com';
-$user = 'ithxzpubsdyssh';
-$passwd = 'yzRs8R1aJkymNawqYGJkS_4ySJ';
-$db = 'd3q71k9f5t7k2c';
-$port = '5432';
-$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd sslmode='allow'" ;
-?>
