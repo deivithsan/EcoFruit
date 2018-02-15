2603e4285567c7a0549448bab0ca7da2d04e17b9
Deivith Becerra <deivith.1@hotmail.com>
2018-02-14 19:57:09 -0500

    Add files via upload
    correcion al titulo de la pagina y actualizado con el que es.

--- production/perfil.php
+++ production/perfil.php
@@ -21,7 +21,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ingresar Información del Usuario</title>
+    <title>Perfil</title>
     <link rel="shortcut icon" href="../img/icono.ico">
     <!-- Bootstrap -->
     <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
