4e71899ef3af6233bf9f47aeabbeb52be448a986
Deivith Becerra <deivith.1@hotmail.com>
2018-04-02 18:38:25 -0500

    Add files via upload

--- production/estadisticasProd.php
+++ production/estadisticasProd.php
@@ -151,7 +151,7 @@ $nombreyapellido = $admin->get_NombreApellido();
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Ventas</h2>
+                            <h2>Productos</h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
